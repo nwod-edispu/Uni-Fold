7cf27ef532bb37f28b9bac8099ee557c4b3174f6
hjzs <2113984236@qq.com>
2022-02-22 16:10:43 +0800

    test

--- unifold/train/utils.py
+++ unifold/train/utils.py
@@ -134,6 +134,8 @@ def crop_and_pad(
 
         raw_features = {k: pad(k, v) for k, v in raw_features.items()}
         raw_labels = {k: pad(k, v) for k, v in raw_labels.items()}
+        print("utils", raw_labels['aatype_index'].shape, raw_labels['all_atom_positions'].shape,
+              raw_labels['all_atom_mask'].shape)
     elif num_res > crop_size:
         # crop long seq.
         np.random.seed(random_seed)
