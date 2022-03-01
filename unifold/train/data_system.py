96fc98df1494b2cc2ccb450e7a05575626a30591
hjzs <2113984236@qq.com>
2022-03-01 16:11:29 +0800

    show type

--- unifold/train/data_system.py
+++ unifold/train/data_system.py
@@ -187,11 +187,12 @@ class DataSystem:
         """
         Add by hj, for computing the features ahead to accelerate the data loader
         """
-        for index in range(6700, self.num_prot):
+        for index in range(6700, 6701):
             if index % 100 == 0:
                 print(index)
             try:
                 _, rng, batch = self.get_batch(index, rng)
+                print(type(batch))
             except (IndexError, UnboundLocalError):
                 print("prot_name: ", self.prot_keys[index % self.num_prot])
                 continue
