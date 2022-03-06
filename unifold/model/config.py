1e72eb624144dc36f743831d4a2cf56c6e6a634e
hjzs <2113984236@qq.com>
2022-03-07 00:21:56 +0800

    unifold

--- unifold/model/config.py
+++ unifold/model/config.py
@@ -53,8 +53,10 @@ CONFIG_DIFFS = {
         'model.embeddings_and_evoformer.template.enabled': True,
         'model.heads.experimentally_resolved.weight': 0.0,
         'model.heads.structure_module.structural_violation_loss_weight': 0.0,
-        'data.common.max_extra_msa': 256
+        # 'data.common.max_extra_msa': 256,
         # 'data.eval.crop_size': 128
+        'model.embeddings_and_evoformer.evoformer_num_block': 24,
+        'model.heads.structure_module.num_layer': 4
     },
     # A demo configuration for debugging:
     'demo': {
